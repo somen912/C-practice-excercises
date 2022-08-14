d367e21d0024a92b44aa1ed2ae4d30a4660552fd
Somen Das <71995297+somen912@users.noreply.github.com>
2022-08-14 20:34:41 +0530

    Add files via upload
    This excercise is about how to write program to read two strings from the keyboard using the ReadLine statements and display them on one line using two Write statements.

---
+++ Excercise_1.cs
@@ -0,0 +1,28 @@
+﻿/* Write a program to read two strings from keyboard using the Readline statements 
+and display them on one line using two write statement*/
+
+
+
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {  
+            Console.WriteLine("Enter first sentence");
+            string line1=Console.ReadLine();
+            Console.WriteLine("Enter second sentence");
+            string line2=Console.ReadLine();
+
+            Console.WriteLine("\nOutput:");
+            Console.WriteLine($"{line1} {line2}");
+
+           
+
+         
+        }
+    }
+}
