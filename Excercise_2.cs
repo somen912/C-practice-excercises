828003a5be9fc9e5c820bb72f08add2515151e83
Somen Das <71995297+somen912@users.noreply.github.com>
2022-08-14 21:33:12 +0530

    Add files via upload

---
+++ Excercise_2.cs
@@ -0,0 +1,32 @@
+﻿/* Write a program that takes the line of text 
+   John F Kennedy
+   as a command line input and display the following output
+   Kennedy John F */
+
+
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {  
+           
+
+            Console.WriteLine("Enter Name");
+            string line1=Console.ReadLine();
+            
+            string[] word=line1.Split();
+
+            string final= word[2]+" "+word[0]+" "+word[1];
+            Console.WriteLine($"\nOutput:");
+            Console.WriteLine($"{final}");
+
+           
+
+         
+        }
+    }
+}
