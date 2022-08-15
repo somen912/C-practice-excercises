94a0f884f90e98ddd00556a120d6461ee1c839f1
Somen Das <71995297+somen912@users.noreply.github.com>
2022-08-15 11:22:52 +0530

    Add files via upload

---
+++ Exercise_3.cs
@@ -0,0 +1,48 @@
+﻿/* Define two classes, one with a method to display the string "C sharp"
+   and the other to display the string "programming".
+   Write a program using these classes to display a single line output as follows
+   "C sharp programming" */
+
+
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    public class student1
+    {
+        public string print (string a)
+        {
+           // Console.WriteLine($"{a}");
+           return a;
+        }
+
+    }
+
+    public class student2
+    {
+        public string print (string a)
+        {
+            //Console.WriteLine($"{a}");
+            return a;
+        }
+
+    }
+
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {  
+
+            student1 somen = new student1();
+            student2 surbhi = new student2();
+
+            Console.WriteLine("\nOutput:");
+            Console.WriteLine($"{somen.print("C sharp")} {surbhi.print("programming")}");
+           
+
+         
+        }
+    }
+}
