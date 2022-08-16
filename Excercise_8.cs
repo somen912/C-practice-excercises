b9965f270eb125d2ae443fc758fd7ca3fc756d2d
Somen Das <71995297+somen912@users.noreply.github.com>
2022-08-16 22:40:00 +0530

    Add files via upload

---
+++ Excercise_8.cs
@@ -0,0 +1,30 @@
+﻿/* Byte addition program */
+
+
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            byte b1 = 100;
+            byte b2 = 200;
+            int c = b1 + b2;   // byte + byte = int
+
+            Console.WriteLine(c);
+           
+        
+                    
+
+                    
+                   
+                
+            
+         
+        }
+    }
+}
