631f67e18307520689dfa1afaf84b24b16cd096a
Somen Das <71995297+somen912@users.noreply.github.com>
2022-08-16 22:23:52 +0530

    Add files via upload

---
+++ Excercise_7.cs
@@ -0,0 +1,46 @@
+﻿/* Program for explaining the concept of boxing and unboxing */
+
+
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int a = 10;
+            string b = "somen";
+            double d = 100.125;
+
+            Console.WriteLine($"\nType of a : {a.GetType()}\n Type of b : {b.GetType()}\n Type of c: {d.GetType()}");
+
+            // Boxing 
+
+            object e = a;
+            object f = b;
+            object g = d;
+
+            Console.WriteLine($"\nType of e : {e.GetType()}\n Type of f : {f.GetType()}\n Type of g: {g.GetType()}");
+
+            // Unboxing 
+
+            int h = (int) e;
+            string i =(string) b;
+            double j = (double)d;
+
+            Console.WriteLine($"\nType of h : {h.GetType()}\n Type of i : {i.GetType()}\n Type of j: {j.GetType()}");
+           
+        
+                    
+
+                    
+                   
+                
+            
+         
+        }
+    }
+}
