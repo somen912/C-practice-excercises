82d9135568fd889169dc5a146daa8b3347602da2
Somen Das <71995297+somen912@users.noreply.github.com>
2022-08-22 22:50:35 +0530

    Add files via upload

---
+++ Excercise_13.cs
@@ -0,0 +1,52 @@
+﻿/* Example of variable argument list using params */
+
+
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+
+    class Converter
+    {
+      public double Average_function(params double[] avg) 
+      {
+        double sum=0;
+
+        foreach(int i in avg)
+        {
+          sum+=i;
+
+        }
+
+        return sum/avg.Length;
+        
+        
+      }
+
+      
+        
+    }
+
+    
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+          double[] container;
+          container = new double[4];
+          Console.WriteLine("Enter numbers");
+
+          for(int i=0; i<container.Length; i++)
+          {
+            container[i]=double.Parse(Console.ReadLine());
+          }
+          Converter somen = new Converter();
+
+          Console.WriteLine("\nOutput:");
+          Console.WriteLine($"{somen.Average_function(container)}");
+          
+         
+        }
+    }
+}
