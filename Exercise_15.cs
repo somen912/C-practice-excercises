e8c6bd7df9815f71d7d1a5c8b29ee6c747eaf2a3
Somen Das <71995297+somen912@users.noreply.github.com>
2022-08-30 21:10:23 +0530

    Add files via upload

---
+++ Exercise_15.cs
@@ -0,0 +1,39 @@
+﻿/* Program to for guessing the magic word */
+
+
+using System;
+using System.Collections;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+
+    
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+          Console.WriteLine("**Guess the magic word**");
+          string str1 = "somen";
+
+          Console.WriteLine("Enter your world");
+          string str2 = Console.ReadLine();
+
+          bool b1 = String.Equals(str1,str2);
+
+          if(b1==true) 
+          {
+            Console.WriteLine("\nBingo!!! you found the magic word");
+          }
+          else
+          {
+            Console.WriteLine($"\nWrong Guess!!!\nHint: Length of magic word is: {str1.Length}");
+          }
+
+          
+          
+         
+        }
+    }
+} 
