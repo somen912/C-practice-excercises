b0a0326d377d78c18ccf9d7c645301a1ab5d3d83
Somen Das <71995297+somen912@users.noreply.github.com>
2022-09-04 16:14:16 +0530

    Add files via upload

---
+++ Exercise_17.cs
@@ -0,0 +1,52 @@
+﻿/* A simple example of struct and enum */
+
+
+using System;
+using System.Collections;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+
+  struct converter           //    defining struct
+  {
+    public enum shape{circle, square};  //enumeration
+
+
+    public void showArea(int a, shape b)
+    {
+      switch (b)
+      {
+        case shape.circle:
+                  Console.WriteLine($"Area of {shape.circle}: {Math.PI*(a*a)}");
+                  break;
+        case shape.square:
+                  Console.WriteLine($"Area of {shape.square}: {a*a}");
+                  break;          
+        default:
+                  break;          
+                
+      }
+    }
+  }
+
+    
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+          converter somen;
+
+          somen.showArea(12,converter.shape.circle);
+          somen.showArea(12,converter.shape.square);
+
+          
+
+          
+          
+          
+         
+        }
+    }
+} 
